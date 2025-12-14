8f558862a0ef8f87df58cb35aa1aa722623f2e99
TuaBBL <genetua@gtrax.net>
2025-12-15 06:38:38 +1000

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import logo from "../assets/logo-beatbookingslive.png";
 
 export default function Header() {
   return (
-    <header className="relative z-50 bg-black">
-      <div className="flex items-center justify-between px-6 py-4">
-        {/* Logo */}
+    <header className="sticky top-0 z-30 bg-black">
+      <div className="flex items-center justify-between px-6 py-6">
+        {/* Logo (Home Button) */}
         <a href="/" className="flex items-center">
-  <img
-  src={logo}
-  alt="BeatBookingsLive"
-  className="h-20 md:h-24 object-contain drop-shadow-[0_0_16px_rgba(57,255,20,0.65)]"
-/>
-</a>
+          <img
+            src={logo}
+            alt="BeatBookingsLive"
+            className="h-28 md:h-32 object-contain cursor-pointer drop-shadow-[0_0_22px_rgba(57,255,20,0.75)]"
+          />
+        </a>
 
         {/* Domain */}
         <div className="text-sm md:text-base text-gray-300 tracking-wide">
@@ -20,7 +20,4 @@ export default function Header() {
       </div>
 
       {/* Red divider */}
-      <div className="h-px w-full bg-[#ff2d2d] shadow-[0_0_8px_rgba(255,45,45,0.7)]" />
-    </header>
-  );
-}
+      <div className="h-px w-full bg-neon-red shadow-
